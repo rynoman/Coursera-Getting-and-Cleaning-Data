3f4d06c72e402d7eaf87416c0d96df44f43514f6
rynoman <rohanthinks@hotmail.com>
2016-04-11 02:41:52 +1000

    Submitting final code_v2

--- run_analysis.R
+++ run_analysis.R
@@ -2,21 +2,59 @@
 
 # reshape2 is an R package written by Hadley Wickham that makes it easy to 
 # transform data between wide and long formats.
+install.packages("reshape2")
+library(reshape2)
 
-# Make sure "reshape2" is installed & inform user to install if it's missing. 
-if (!("reshape2" %in% rownames(installed.packages())) ) {
-        print("Missing package - reshape2. Please install as this script usage the package.")
-        }
+# Importing data from UCI HAR Dataset directory
+testData <- read.table("./UCI HAR Dataset/test/X_test.txt")
+testSubjects <- read.table("./UCI HAR Dataset/test/subject_test.txt")
+testActivities <- read.table("./UCI HAR Dataset/test/y_test.txt")
+
+trainData <- read.table("./UCI HAR Dataset/train/X_train.txt")
+trainSubjects <- read.table("./UCI HAR Dataset/train/subject_train.txt")
+trainActivities <- read.table("./UCI HAR Dataset/train/y_train.txt")
+
+featuresList <- read.table("./UCI HAR Dataset/train/X_train.txt")
+
+# Assign column names
+colnames(testActivities) <- "Activity"
+colnames(trainActivities) <- "Activity"
+colnames(testSubjects) <- "Subject"
+colnames(trainSubjects) <- "Subject"
+
+# Remove punctuations from feature names
+colNames <- gsub("[[:punct:]]","", featuresList$V2, ignore.case = FALSE, perl = FALSE)
+
+# Assign features as column names to test and train dataset
+colnames(testData) <- colNames
+colnames(trainData) <- colNames
+
+# Merging the training and the test sets to create one data set.
+
+testDataMerged <- cbind(testSubjects, testActivities,testData)
+trainDataMerged <- cbind(trainSubjects, trainActivities,trainData)
+completeData <- rbind(testDataMerged, trainDataMerged)
 
 
-Step 1: Merges the training and the test sets to create one data set.
+# Extracting only the measurements on the mean and standard deviation for each measurement.
 
-Step 2: Extracts only the measurements on the mean and standard deviation for 
-each measurement.
+meanFiltered <- grep("mean",names(completeData),ignore.case=TRUE)
+mean_colNames <- names(completeData)[meanFiltered]
+stdFiltered <- grep("std",names(completeData),ignore.case=TRUE)
+std_colNames <- names(completeData)[stdFiltered]
 
-Step 3: Uses descriptive activity names to name the activities in the data set
+completeDataFiltered <- completeData[, c("Subject","Activity", mean_colNames, std_colNames)]
 
-Step 4: Appropriately labels the data set with descriptive variable names.
+# Assigned descriptive activity names to name the activities in the data set
+activityLabels <- read.table("./UCI HAR Dataset/activity_labels.txt", header=FALSE, col.names=c('ID', 'Activity'))
 
-Step 5: From the data set in step 4, creates a second, independent tidy data set 
-with the average of each variable for each activity and each subject.
+
+# Appropriately labeled the data set with descriptive variable names.
+completeDataFiltered_byActivity <- merge(activityLabels, completeDataFiltered,by.x="ID",by.y="Activity",all=TRUE)
+completeDataFiltered_byActivity$ID <- NULL
+tidyData <- melt(completeDataFiltered_byActivity, id.vars = c("Subject","Activity"))
+
+# Step 5: From the data set in step 4, creates a second, independent tidy data set 
+# with the average of each variable for each activity and each subject.
+tidyData_Mean <- dcast(tidyData, Subject + Activity ~ variable, mean)
+write.table(tidyData_Mean,"./Mean_TidyData_Subject.txt")
